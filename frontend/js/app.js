798def6845ac5a3a37d85240d7110017c6990c73
germanf <ferreyra.ger@gmail.com>
2016-08-05 00:38:47 -0300

    + flight search functionality

--- frontend/js/app.js
+++ frontend/js/app.js
@@ -1,9 +1,144 @@
-(function(window, document, $, undefined){
+(function (window, document, $, moment, undefined) {
+  "use strict";
 
-  $(function(){
+  // cache common elements
+  var $win = $(window);
+  var $doc = $(document);
+  var $body = $('body');
 
-    // document ready
+  // endpoint base url
+  var baseUrl = "http://localhost:3000/api/v1/public/flight/"; // the script where you handle the form input.
 
+  // Site Preloader
+  // -----------------------------------
+  NProgress.start();
+
+  $(function () {
+    NProgress.done();
+    $body.addClass('site-loaded');
   });
 
-})(window, document, window.jQuery);
+  //
+  // Search
+  //
+  /**
+   * get a single deferred
+   * @param flighSearchUrl
+   * @param params
+   * @returns {*}
+   */
+  var deferFlightSearch = function (flighSearchUrl, params) {
+    return $.get(flighSearchUrl, params);
+  };
+
+  /**
+   * Build all flight searchs for the given departure date, airlines, origins and destinations
+   * @param urlBase
+   * @param departureDate
+   * @param airlines
+   * @param origins
+   * @param destinations
+   * @returns {*}
+   */
+  var buildFlightSearchs = function (urlBase, departureDate, airlines, origins, destinations) {
+    var flightRequests = [];
+
+    airlines.map(function (airline) {
+      var flighSearchUrl = urlBase + airline.code;
+
+      return origins.map(function (origin) {
+        return destinations.map(function (destination) {
+          var params = {
+            date: departureDate,
+            from: origin.airportCode,
+            to: destination.airportCode,
+          };
+
+          flightRequests.push(deferFlightSearch(flighSearchUrl, params));
+        })
+      })
+    });
+
+    return flightRequests;
+  };
+
+  /**
+   * exec call for the given departure date
+   */
+  var execFlightSearchs = function (urlFlightSearchBase, departureDate, airlines, origins, destinations) {
+    var flightsInDepartureDay = buildFlightSearchs(urlFlightSearchBase, departureDate, airlines, origins, destinations);
+    $.when.all(flightsInDepartureDay)
+      .then(function (results) {
+        results.map(function (result) {
+          var flight = JSON.parse(result[0]);
+          console.log(flight);
+        });
+      });
+  }
+
+  /**
+   * exec the all flight searchs, +-2 days from departure day
+   * @param urlFlightSearchBase
+   * @param departureDate
+   * @param airlines
+   * @param origins
+   * @param destinations
+   */
+  var execAllFlightSearchs = function (urlFlightSearchBase, departureDate, airlines, origins, destinations) {
+    var twoDaysBefore = moment(departureDate, 'YYYY-MM-DD').add('days', -2);
+    var oneDayBefore = moment(departureDate, 'YYYY-MM-DD').add('days', -1);
+    var oneDayAfter = moment(departureDate, 'YYYY-MM-DD').add('days', 1);
+    var twoDaysAfter = moment(departureDate, 'YYYY-MM-DD').add('days', 2);
+
+    execFlightSearchs(urlFlightSearchBase, departureDate, airlines, origins, destinations);
+    execFlightSearchs(urlFlightSearchBase, oneDayBefore, airlines, origins, destinations);
+    execFlightSearchs(urlFlightSearchBase, twoDaysBefore, airlines, origins, destinations);
+    execFlightSearchs(urlFlightSearchBase, oneDayAfter, airlines, origins, destinations);
+    execFlightSearchs(urlFlightSearchBase, twoDaysAfter, airlines, origins, destinations);
+  }
+
+  /**
+   * Flight search submit form
+   */
+  $("#form-search").submit(function (e) {
+    var urlAirlines = baseUrl + 'airlines',
+      urlAirport = baseUrl + 'airports',
+      originAirport = $('#origin-airport').val(),
+      destinationAirport = $('#destination-airport').val(),
+      urlFlightSearchBase = baseUrl + 'search/',
+      departureDate = moment($('#departure-date').val()).format('YYYY-MM-DD');
+
+    // get all airlines & airports based on origin and destination inputs
+    $.when(
+      $.get(urlAirlines),
+      $.get(urlAirport, {q: originAirport}),
+      $.get(urlAirport, {q: destinationAirport})
+    ).then(function (airlines, origins, destinations) {
+      airlines = JSON.parse(airlines[0]);
+      origins = JSON.parse(origins[0]);
+      destinations = JSON.parse(destinations[0]);
+
+      execAllFlightSearchs(urlFlightSearchBase, departureDate, airlines, origins, destinations);
+    });
+
+    e.preventDefault(); // avoid to execute the actual submit of the form.
+  });
+
+  // jQuery resolve all promises
+  if (jQuery.when.all === undefined) {
+    jQuery.when.all = function (deferreds) {
+      var deferred = new jQuery.Deferred();
+      $.when.apply(jQuery, deferreds).then(
+        function () {
+          deferred.resolve(Array.prototype.slice.call(arguments));
+        },
+        function () {
+          deferred.fail(Array.prototype.slice.call(arguments));
+        });
+
+      return deferred;
+    }
+  }
+
+})
+(window, document, window.jQuery, moment);
